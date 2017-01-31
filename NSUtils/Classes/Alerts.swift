363a203093607e5ef7992b3f3f71a916645b2a26
Jack Colley <jack.colley@netsells.co.uk>
2017-01-31 17:01:33 +0000

    Adding default action title to presentSystemAlert

--- NSUtils/Classes/Alerts.swift
+++ NSUtils/Classes/Alerts.swift
@@ -11,15 +11,14 @@ import Foundation
 public protocol Presentable {}
 
 extension Presentable {
-    public func showSystemAlert(title: String, message: String) {
+    public func showSystemAlert(title: String, message: String, defaultActionTitle: String = "OK") {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
         
-        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
+        let okAction = UIAlertAction(title: defaultActionTitle, style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
         }
         
         alertController.addAction(okAction)
         
         UIApplication.shared.keyWindow?.rootViewController?.present(alertController, animated: true, completion: nil)
     }
-
 }
